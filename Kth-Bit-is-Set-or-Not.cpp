2bec6e510c11ffce893ee513d31b7e8a11b7cedf
DineshSirvicodes <112575008+DineshSirvicodes@users.noreply.github.com>
2025-12-04 19:47:10 +0530

    Update Kth-Bit-is-Set-or-Not.cpp

--- Kth-Bit-is-Set-or-Not.cpp
+++ Kth-Bit-is-Set-or-Not.cpp
@@ -22,3 +22,41 @@ class Solution {
         return res[res.length()-k-1] == '1';
     }
 };
+
+// TC - O(logn),due to integer-to-binary conversion and indexing. , SC - O(logn) due to binary string |`
+
+class Solution {
+  public:
+    bool checkKthBit(int n, int k) {
+        //  code here
+        bool flag = false;
+        int index = 0;
+        int res = 0;
+        while(n > 0){
+                res = n%2;
+                if(res == 1 && index == k){
+                    flag = true;
+                    break;
+                }
+            
+            n /=2;
+            index++;
+        }
+        return flag;
+    }
+};
+
+
+
+// Tc - O(1) , SC - O(1)
+
+class Solution {
+  public:
+    bool checkKthBit(int n, int k) {
+        //  code here
+      // Check if the i-th bit is set using bitwise AND operation
+        int leftShift = 1 << k;
+       // If the i-th bit is 1, the result will be non-zero
+        return (n & leftShift) != 0;
+    }
+};
