cb8bbcce08b04d0c82f6fd970d7623b0926342f6
DineshSirvicodes <112575008+DineshSirvicodes@users.noreply.github.com>
2025-12-07 22:05:23 +0530

    Update CountNumberOfSetBits.cpp

--- CountNumberOfSetBits.cpp
+++ CountNumberOfSetBits.cpp
@@ -18,3 +18,35 @@ class Solution {
 };
 
 
+
+class Solution {
+  public:
+    int setBits(int n) {
+        // Write Your Code here
+        int cnt = 0;
+        while(n>0){
+            if(n&1){ // checks last bit is set or not
+                cnt++;
+            }
+            n=n>>1;
+        }
+        return cnt;
+    }
+};
+
+
+// Brian Kernighan’s Algorithm
+//Each time you do n = n & (n - 1), you remove ONE 1 from n.
+
+class Solution {
+  public:
+    int setBits(int n) {
+        // Write Your Code here
+        int cnt = 0;
+        while(n>0){
+            n = n & (n-1);
+            cnt++;
+        }
+        return cnt;
+    }
+};
